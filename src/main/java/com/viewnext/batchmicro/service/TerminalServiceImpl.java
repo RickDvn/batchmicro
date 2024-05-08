03e690efe6ea0f40d30a33b62d69d6a001db149e
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-08 12:31:14 +0200

    Service Implementado

--- src/main/java/com/viewnext/batchmicro/service/TerminalServiceImpl.java
+++ src/main/java/com/viewnext/batchmicro/service/TerminalServiceImpl.java
@@ -1,5 +1,20 @@
 package com.viewnext.batchmicro.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.viewnext.batchmicro.model.Terminal;
+import com.viewnext.batchmicro.reader.TerminalReader;
+
+@Service
 public class TerminalServiceImpl implements TerminalService {
+
+	@Override
+	public List<Terminal> getterminales() {
+		List<Terminal> terminales;
+		terminales = TerminalReader.readCSVsTerminal();
+		return terminales;
+	}
 
 }
