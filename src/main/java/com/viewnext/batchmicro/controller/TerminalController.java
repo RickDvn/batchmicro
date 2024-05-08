5a31fa09dc651065f4f7f7aae06ca57d0b079367
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-08 12:31:25 +0200

    Controller implementado

--- src/main/java/com/viewnext/batchmicro/controller/TerminalController.java
+++ src/main/java/com/viewnext/batchmicro/controller/TerminalController.java
@@ -1,5 +1,24 @@
 package com.viewnext.batchmicro.controller;
 
-public class TerminalController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viewnext.batchmicro.model.Terminal;
+import com.viewnext.batchmicro.service.TerminalService;
+
+@RestController
+@RequestMapping("/terminales")
+public class TerminalController {
+	
+	@Autowired
+	private TerminalService terminalService;
+	
+	@GetMapping(value = {"", "/"})
+	public List<Terminal> getTerminales(){
+		return terminalService.getterminales();
+	}
 }
