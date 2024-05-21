a82a8cf4ec844fb622df5dce71ab71839f904ff5
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-21 09:41:36 +0200

    Update TerminalController.java

--- src/main/java/com/viewnext/batchmicro/controller/TerminalController.java
+++ src/main/java/com/viewnext/batchmicro/controller/TerminalController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.viewnext.batchmicro.model.Terminal;
 import com.viewnext.batchmicro.service.TerminalService;
